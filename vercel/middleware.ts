b3897754ab88c4764b204a2b4a4b5a3b0bcc5a48
awaismalik01 <mawais_malik@hotmail.com>
2026-07-25 02:29:07 -0400

    Log

--- vercel/middleware.ts
+++ vercel/middleware.ts
@@ -25,9 +25,11 @@ export default function middleware(req: Request) {
       request: { headers: newHeaders },
     });
   }
-
+  console.log(cookie);
   const tokenCookie = cookie.find((c) => c.startsWith("auth_token="));
+  console.log(tokenCookie);
   const token = tokenCookie?.substring("auth_token=".length);
+  console.log(token);
 
   if (!token) {
     return new Response("Access Denied", { status: 403 });
